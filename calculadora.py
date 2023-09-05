59bac2d52c2e246404ca9a55a998c0f447567939
Thiago Monteiro <thiago.mont.vieira@gmail.com>
2023-09-04 21:14:22 -0400

    first change

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,5 @@
-#-treinando python- CALCULADORA SIMPLES, REALIZANDO AS 4 OPERACOES BASICAS
+#04092023-treinando python- CALCULADORA SIMPLES, REALIZANDO AS 4 OPERACOES BASICAS
+#04092023 - adicionando potencia e raiz quadrada
 
 import math as m
 
@@ -7,6 +8,8 @@ op = int(input("""Você pode realizar uma das quatros operações básicas abaix
            2 - Subtração
            3 - Multiplicação
            4 - Divisão
+           5 - Potênciação
+           6 - Raiz quadrada
            
            Qual operação deseja: """))
 if op == 1:
@@ -36,7 +39,19 @@ elif op == 4:
     
     s = n1 / n2
     print(f"{n1} / {n2} = {s:.2f}")
-   
+
+elif op == 5:
+    n1 = int(input("Informe a base: "))
+    n2 = int(input("Informe o expoente: "))
+      
+    s = n1 ** n2
+    print(f"{n1} elevado à {n2} é {s}")
+
+elif op == 6:
+    n1 = int(input("Informe o valor que deseja a raiz: "))
+    s = m.sqrt(n1)
+    print(f"A raiz quadrade de {n1} é {s}")
+        
 else:
     print("Comando invalido!")
 
